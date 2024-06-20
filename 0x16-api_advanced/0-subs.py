b78739abaf72eb38fd4524d4d892403cd974e7c8
CorneliusNgure <kingcornelius07@gmail.com>
2024-06-20 13:40:08 -0400

    0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -9,16 +9,13 @@ def number_of_subscribers(subreddit):
         Queries the Reddit API and returns the number of subscribers
         (not active users, total subscribers) for a given subreddit.
     """
-    url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    headers = {'User-Agent': 'Google Chrome Version 126.0.6478.115'}
 
-    try:
-        response = requests.get(url, headers=headers, allow_redirects=False)
-
-        if response.status_code == 200:
-            data = response.json()
-            return data['data']['subscribers']
-        if response == 404:
-            return 0
-    except Exception:
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {
+        "User-Agent": "Google Chrome Version 126.06478.115"
+    }
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code == 404:
         return 0
+    results = response.json().get("data")
+    return results.get("subscribers")
