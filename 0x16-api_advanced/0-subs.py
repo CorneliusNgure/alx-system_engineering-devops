f8fb9bc8e781bcc2b162fb6e0350b9c1a0e0a9d9
CorneliusNgure <kingcornelius07@gmail.com>
2024-06-20 13:34:44 -0400

    0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -10,7 +10,7 @@ def number_of_subscribers(subreddit):
         (not active users, total subscribers) for a given subreddit.
     """
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    headers = {'User-Agent': 'Mozilla/5.0'}
+    headers = {'User-Agent': 'Google Chrome Version 126.0.6478.115'}
 
     try:
         response = requests.get(url, headers=headers, allow_redirects=False)
