fc0da0da119f9d892f77f7486eb1031cb942d22a
CorneliusNgure <kingcornelius07@gmail.com>
2024-06-20 13:30:12 -0400

    0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -18,7 +18,7 @@ def number_of_subscribers(subreddit):
         if response.status_code == 200:
             data = response.json()
             return data['data']['subscribers']
-        else:
+        if response == 404:
             return 0
     except Exception:
         return 0
